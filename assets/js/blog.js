d7851912d0a132499feb73eaa90c03c492f9d774
Salokin <nicholas.dpoul@gmail.com>
2024-04-06 21:26:03 -0400

    started light/dark mode: set event listener in 1st page  JS, set light/dark formats for 1st page background and borders

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,1 +1,15 @@
-console.log("test");
+const themeChange = document.querySelector('#themeChange');
+const container = document.querySelector('.container')
+
+let mode = 'light';
+
+themeChange.addEventListener('click', function () {
+    if (mode === 'light') {
+        mode = 'dark';
+        container.setAttribute('class', 'dark');
+    }
+    else {
+        mode = 'light';
+        container.setAttribute('class', 'light')
+    }
+})
