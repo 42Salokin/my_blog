a09c02efc7b5655fc2e27e3786a3923e94c00f3a
Salokin <nicholas.dpoul@gmail.com>
2024-04-07 07:17:12 -0400

    finished light/dark mode formatting for 1st page, centered button and added label

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,14 +1,19 @@
 const themeChange = document.querySelector('#themeChange');
-const container = document.querySelector('.container')
+const themeChange2 = document.querySelector('#themeChange2');
+const container = document.querySelector('.container');
+const label = document.querySelector('#label');
 let mode = 'light';
+document.getElementById(themeChange2).style.display = "none"
 
 themeChange.addEventListener('click', function () {
     if (mode === 'light') {
         mode = 'dark';
         container.setAttribute('class', 'dark');
+        label.textContent = "Dark";
     }
     else {
         mode = 'light';
         container.setAttribute('class', 'light')
+        label.textContent = "Light";
     }
 })
