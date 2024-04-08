ae1758fd51314fe0f12e4ca2597b56f6b4b364ce
Salokin <nicholas.dpoul@gmail.com>
2024-04-07 20:20:05 -0400

    made light button switch to dark button and back when clicked, finished light/dark mode on 1st page

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,19 +1,19 @@
-const themeChange = document.querySelector('#themeChange');
-const themeChange2 = document.querySelector('#themeChange2');
+const themeChange = document.querySelector('.themeChange');
 const container = document.querySelector('.container');
 const label = document.querySelector('#label');
 let mode = 'light';
-document.getElementById(themeChange2).style.display = "none"
 
 themeChange.addEventListener('click', function () {
     if (mode === 'light') {
         mode = 'dark';
         container.setAttribute('class', 'dark');
         label.textContent = "Dark";
+        themeChange.setAttribute('class', 'em em-cityscape themeChange');
     }
     else {
         mode = 'light';
-        container.setAttribute('class', 'light')
+        container.setAttribute('class', 'light');
         label.textContent = "Light";
+        themeChange.setAttribute('class', 'em em-city_sunrise themeChange');
     }
 })
